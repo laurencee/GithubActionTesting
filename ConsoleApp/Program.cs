6252dd0f9df5d28f60d32dd4e9d36636dd9cb4ab
Laurence Evans <laurence.evanz@gmail.com>
2022-07-21 05:50:45 +1000

    Test actions firing

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("This change wont break the build!");
+Console.WriteLine("Test actions firing");
