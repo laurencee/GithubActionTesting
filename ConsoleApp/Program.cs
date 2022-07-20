5d92df33b26c6a0248fd0e4bd774c8f00a078f35
Laurence Evans <laurence.evanz@gmail.com>
2022-07-21 05:27:30 +1000

    Non-breaking change

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("I add value!");
+Console.WriteLine("This change wont break the build!");
